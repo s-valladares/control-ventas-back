22a11417263e6dfea6d8c6dd14dd317897958cc7
Salvador Valladares <salva.1416@gmail.com>
2020-02-15 17:39:58 -0600

    Crud productos completo

--- src/main/java/com/solucionesti/controlventas/ventasback/VentasBackApplication.java
+++ src/main/java/com/solucionesti/controlventas/ventasback/VentasBackApplication.java
@@ -2,8 +2,10 @@ package com.solucionesti.controlventas.ventasback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VentasBackApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/solucionesti/controlventas/ventasback/resources/ProductoController.java
+++ src/main/java/com/solucionesti/controlventas/ventasback/resources/ProductoController.java
@@ -5,14 +5,16 @@ import com.solucionesti.controlventas.ventasback.domains.Producto;
 import com.solucionesti.controlventas.ventasback.services.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(UrlBaseApi.URL_API)
@@ -25,6 +27,115 @@ public class ProductoController {
     private IProductoService objService;
 
     @GetMapping(entidad)
-    public List<Producto> index() { return objService.getAll(); }
+    public ResponseEntity<?> index() {
+        List<Producto> objNew = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            objNew = objService.getAll();
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al obtener de la base de datos");
+            response.put("error", ex.getMessage().concat(": ").concat(ex.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("size", objNew.size());
+        response.put("rows", objNew);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping(entidad + "/{id}" )
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Producto obj = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            obj = objService.getId(id);
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al consultar base de datos");
+            response.put("error", ex.getMessage().concat(": ").concat(ex.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (obj == null) {
+            response.put("mensaje", entidad.toUpperCase() + " ID: ".concat(id.toString().concat(" No existe en la base de datos")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Producto>(obj, HttpStatus.OK);
+    }
+
+    @PostMapping(entidad)
+    public ResponseEntity<?> create(@Valid @RequestBody Producto x, BindingResult result) {
+
+        Producto objNew = null;
+        Map<String, Object> response = new HashMap<>();
+
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors().stream().map(err -> {
+                return "El campo '" + err.getField() + "' " + err.getDefaultMessage();
+            }).collect(Collectors.toList());
+
+            response.put("errors", errors);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            //System.out.print(x.getApellidos());
+            objNew = objService.create(x);
+
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al insertar en la base de datos");
+            response.put("error", ex.getMessage().concat(": ").concat(ex.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "OK");
+        response.put("RES", objNew);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping(entidad + "/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody Producto fl, BindingResult result, @PathVariable Long id) {
+
+        Producto flActual = objService.getId(id);
+        Producto flActualizado = null;
+        Map<String, Object> response = new HashMap<>();
+
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors().stream().map(err -> {
+                return "El campo '" + err.getField() + "'" + err.getDefaultMessage();
+            }).collect(Collectors.toList());
+
+            response.put("errors", errors);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        if (flActual == null) {
+            response.put("mensaje", "Error: no hay objeto con id: "
+                    .concat(id.toString().concat(". No existe en la base de datos")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+
+            flActual.setNombre(fl.getNombre());
+            flActual.setDescripcion(fl.getDescripcion());
+            flActual.setPrecio(fl.getPrecio());
+            flActual.setTipo(fl.getTipo());
+
+            flActualizado = objService.create(flActual);
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error actualizar el objeto en la base de datos");
+            response.put("error", ex.getMessage().concat(": ").concat(ex.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El objeto fue actualizado correctamente");
+        response.put("RES", flActualizado);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
 
 }
